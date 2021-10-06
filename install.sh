46d400c9e079a182a8a754b4e0fad6e8827c6148
myson1515 <mwl4263@rit.edu>
2021-10-06 12:48:34 -0400

    alexis rc and install added

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+unzip iTerm2*.zip
+mv -R iTerm.app /Applications/
+cp alexis.zshrc ~/.zshrc
+# open README.html
+
